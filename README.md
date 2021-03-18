27257274142c97fa0bb021d2bc9cbb32e047cbb3
lukkas-lukkas <66659328+lukkas-lukkas@users.noreply.github.com>
2021-03-18 14:09:56 -0300

    Exclusão de arquivos do firebase storege

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# Dropbox Clone
+<h1> Dropbox Clone </h1>
 
-## Projeto desenvolvido em Node.js
+<h3> Projeto desenvolvido em Node.js </h3>
 
-### Detalhes
-Projeto desenvolvido em node.js
-Utilização de Express para inicialização do projeto
-Desenvolvimento orientado a objetos
-Utilização de firebase como banco de dados
-Navegação entre pastar
-Upload de arquivos dentro do firebase storage
+<p><strong>Detalhes</strong></p>
+
+<p> Projeto desenvolvido em Node.js </p>
+<p> Utilização de Express para inicialização do projeto </p>
+<p> Desenvolvimento orientado a objetos </p>
+<p> Utilização de firebase realtime database como banco de dados </p>
+<p> Navegação entre pastas </p>
+<p> Upload de arquivos em firebase storage </p>
--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -23,24 +23,85 @@ class DropBoxController {
         return this.listFilesEl.querySelectorAll('.selected');
     }
 
+    removeFolderTask(ref, name){
+
+        return new Promise((resolve, reject)=>{
+            let folderRef = this.getFirebaseRef(`${ref}/${name}`);
+
+            folderRef.on('value', snapshot=>{
+                folderRef.off('value');
+
+                snapshot.forEach(item=>{
+                    let data = item.val();
+                    data.key = item.key;
+
+                    if(data.type === 'folder'){
+                        this.removeFolderTask(`${ref}/${name}`, data.name).then(()=>{
+                            resolve({
+                                fields: {
+                                    key: data.key
+                                }
+                            })
+                        }).catch(error=>{
+                            reject(error);
+                        });
+                    }else if(data.type){
+                        this.removeFile(`${ref}/${name}`, data.name).then(()=>{
+                            resolve({
+                                fields: {
+                                    key: data.key
+                                }
+                            })
+                        }).catch(error=>{
+                            reject(error);
+                        });
+                    }
+                })
+
+                folderRef.remove();
+            })
+        })
+    }
+
     removeTask(){
         let promises = [];
 
         this.getSelection().forEach(li=>{
             let file = JSON.parse(li.dataset.file);
-            let key = li.dataset.key
+            let key = li.dataset.key;
 
-            let formData = new FormData;
+            promises.push(new Promise((resolve, reject)=>{
 
-            formData.append('path', file.path);
-            formData.append('key', key);
+                if(file.type === 'folder'){
+                    this.removeFolderTask(this.currentFolder.join('/'), file.name).then(()=>{
+                        resolve({
+                            fields: {
+                                key
+                            }
+                        });
+                    })
+                } else if(file.type){
+                    this.removeFile(this.currentFolder.join('/'), file.name).then(()=>{
+                        resolve({
+                            fields: {
+                                key
+                            }
+                        });
+                    });
+                }
 
-            let promise = this.ajax('/file', 'DELETE', formData);
 
-            promises.push(promise);
+               
+            }));
         })
 
         return Promise.all(promises);
+    }
+
+    removeFile(ref, name){
+        let fileRef = firebase.storage().ref(ref).child(name);
+
+        return fileRef.delete();
     }
 
     initEvents(){
